f516f5af939da773e2256f4d5b2862d49a032779
denka0087@gmail.com <dubble_bubble>
2017-11-05 20:34:02 +0300

    Lab3 seems to work

--- lab3/helpers.py
+++ lab3/helpers.py
@@ -1,3 +1,5 @@
+from collections import Counter
+
 import cv2 as cv
 import numpy as np
 
@@ -11,3 +13,39 @@ def hist_lines_opencv(im):
         cv.line(h, (x, 0), (x, y), (255, 255, 255))
     y = np.flipud(h)
     return y
+
+
+def count_pixels(img_array):
+    total_count = Counter()
+    for row in img_array:
+        total_count = total_count + Counter(row)
+    return total_count
+
+
+def my_calc_hist(img_array):
+    counted_pixels_by_brightness = count_pixels(img_array)
+    max, min = img_array.max, img_array.min
+    total_amount_of_pixels = float(img_array.size)
+    pixel_probabilities = {
+        brightness: amount / total_amount_of_pixels for brightness, amount in counted_pixels_by_brightness.items()
+    }
+    probs = []
+    for i in range(256):
+        probs.append(pixel_probabilities.get(i, 0))
+    return probs
+
+
+def calc_hist(img_array):
+    max_, min_ = img_array.max(), img_array.min()
+    total_amount_of_pixels = float(img_array.size)
+    counted_pixels_by_brightness = count_pixels(img_array)
+    probs = my_calc_hist(img_array)
+    for i, prob in enumerate(probs):
+        probs[i] = probs[i-1] + prob
+    new_img = img_array.copy()
+    for x, row in enumerate(new_img):
+        for y, pix in enumerate(row):
+            res = ((max_ - min_) * probs[pix] + min_)
+            assert res <= 255
+            new_img[x, y] = res
+    return new_img
--- lab3/main.py
+++ lab3/main.py
@@ -1,30 +1,30 @@
 import numpy as np
 import cv2 as cv
 
-from lab3.helpers import hist_lines_opencv
-
-cv2 = cv
+from lab3.helpers import hist_lines_opencv, my_calc_hist, calc_hist
 from matplotlib import pyplot as plt
 
-image = cv.imread("../clouds.jpg")
+image = cv.imread("../image.png")
 gray_image = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
-
+my_hist = my_calc_hist(gray_image)
+eq_img = calc_hist(gray_image)
 # hist = cv.calcHist(gray_image, 1, None, 255, (0, 255))
 hist = cv.calcHist(gray_image, [1], None, [256], [0, 256])
 
-h = np.zeros((300, 256, 3))
-hist = np.int32(np.around(hist))
-for x, y in enumerate(hist):
-    cv.line(h, (x, 0), (x, y), (255, 255, 255))
-y = np.fliplr(h)
+# h = np.zeros((300, 256, 3))
+# hist = np.int32(np.around(hist))
+# for x, y in enumerate(hist):
+#     cv.line(h, (x, 0), (x, y), (255, 255, 255))
+# y = np.fliplr(h)
 
 
 cv.imshow("Original", image)
 cv.imshow("gist", hist_lines_opencv(gray_image))
 # plt.hist(hist.ravel(), 256, [0, 256])
 # plt.show()
-# cv.imshow("gray", gray_image)
-# cv.imshow("hist", hist)
+cv.imshow("gray", gray_image)
+cv.imshow("my_eq", eq_img)
+cv.imshow("my_eq_hist", hist_lines_opencv(eq_img))
 
 
 
